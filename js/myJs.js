c003f5f5d5d7924879830dddedc91a119476417b
yukito3005 <125259427+yukito3005@users.noreply.github.com>
2023-02-13 19:12:59 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,5 +1,5 @@
 const textConfig = {
-  text1: "He luu cậu!",
+  text1: "Helooo cậu!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
   text3: "Cậu yêu tớ có phải không nào ._.",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
